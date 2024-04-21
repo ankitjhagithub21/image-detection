ca0d3707e4c317b281c46606b54ce7bb4a1a3830
ankitjhagithub21 <ajha45075@gmail.com>
2024-04-21 18:03:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const imageFile = document.getElementById('file')
+const rightImage = document.getElementById('rightImage')
+const magicButton = document.getElementById('magicButton')
+const loader = document.getElementById('loader')
+
+let apiUrl = `https://python-api.techsimplus.com/api/amazon-service/`
+let imageBase64 = null
+
+imageFile.addEventListener('change', async() => {
+    const oneImage = imageFile.files[0]
+    const imageUrl = URL.createObjectURL(oneImage)
+    rightImage.src = imageUrl
+
+
+    //image to base64
+    const reader = new FileReader()
+    reader.readAsDataURL(oneImage)
+
+    reader.onload = () => {
+        imageBase64 = reader.result.split(',')[1]
+        
+    }
+
+
+})
+
+
+
+magicButton.addEventListener('click', async() => {
+    loader.style.display = "flex"
+    const service = document.querySelector('input[name="service"]:checked')?.value
+    if (service == undefined) {
+        alert("Please select correct service")
+        return;
+    }
+    if(imageBase64==null){
+        alert("Please select image")
+        return
+    }
+    const newValues = {
+        service_type : service,
+        image:imageBase64
+    }
+
+    const res = await fetch(apiUrl,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify(newValues)
+    })
+    const resData = await res.json()
+    
+    rightImage.src = resData.data.image
+    loader.style.display ="none"
+})
+
+
